eed1d39f205b9a5eeb535860f77e0e61b636d595
Ivan Porto Carrero <ivan@flanders.co.nz>
2020-01-07 21:41:01 -0800

    cleanup what you create
    Signed-off-by: Ivan Porto Carrero <ivan@flanders.co.nz>

--- gorocksdb.c
+++ gorocksdb.c
@@ -224,6 +224,11 @@ const char* merge_operator_name_fn(void *state)
     return ((bitmap_merge_operator*)state)->name;
 }
 
+void merge_operator_destructor_fn(void *state)
+{
+    free(state);
+}
+
 rocksdb_mergeoperator_t* nflx_bitmap_merger(char *name) {
       struct bitmap_merge_operator *state = malloc(sizeof(*state));
       if (!state) {
@@ -233,7 +238,7 @@ rocksdb_mergeoperator_t* nflx_bitmap_merger(char *name) {
       state->name = name;
       return rocksdb_mergeoperator_create(
         (void *)state,
-        NULL,
+        merge_operator_destructor_fn,
         merge_operator_full_merge_fn,
         NULL,
         NULL,
