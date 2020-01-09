768e49a4054bac8a0cb51b9657b78447ca21f6a2
Ivan Porto Carrero <ivan@flanders.co.nz>
2020-01-08 19:32:32 -0800

    fixes bitmap merger uint32 conversion
    Signed-off-by: Ivan Porto Carrero <ivan@flanders.co.nz>

--- gorocksdb.c
+++ gorocksdb.c
@@ -1,5 +1,5 @@
 #include <memory.h>
-#include <assert.h>
+#include <stdint.h>
 #include "gorocksdb.h"
 #include "roaring.h"
 #include "rocksdb/c.h"
@@ -182,34 +182,35 @@ char* merge_operator_full_merge_fn (
     int num_operands,
     unsigned char *success, size_t *new_value_length)
 {
+    if (num_operands == 0) {
+        new_value_length = &existing_value_length;
+        return (char*)existing_value;
+    }
     roaring_bitmap_t *bmm;
     if (existing_value_length > 0) {
-        bmm = roaring_bitmap_portable_deserialize(existing_value);
+        bmm = roaring_bitmap_portable_deserialize_safe(existing_value, existing_value_length);
     } else {
         bmm = roaring_bitmap_create();
     }
     if (!bmm) {
-        printf("no bitmap created\n");
-        new_value_length = 0;
-        return NULL;
+        new_value_length = &existing_value_length;
+        return (char*)existing_value;
     }
 
-    for (int i = 0; i < *operands_list_length; i++) {
+    for (int i = 0; i < num_operands; i++) {
         if (!operands_list[i]) {
             continue;
         }
-        const char *op = operands_list[i];
-        if (sizeof(op) == 0) {
-            continue;
-        }
+        const unsigned char *op = operands_list[i];
+
         switch (op[0]) {
         case 1:;
-            uint32_t id_to_add = (op[1] << 24) + (op[2]<<16) + (op[3] << 8) + op[4];
+            uint32_t id_to_add = (uint32_t)op[4] | (uint32_t)op[3] << 8 | (uint32_t)op[2] << 16 | (uint32_t)op[1]<<24;
             roaring_bitmap_add(bmm, id_to_add);
             *success = 1;
             break;
         case 2:;
-            uint32_t id_to_remove = (op[1] << 24) + (op[2]<<16) + (op[3] << 8) + op[4];
+            uint32_t id_to_remove = (uint32_t) op[4] | (uint32_t)op[3] << 8 | (uint32_t)op[2]<<16 | (uint32_t)op[1]<<24;
             roaring_bitmap_remove(bmm, id_to_remove);
             *success = 1;
             break;
@@ -218,18 +219,21 @@ char* merge_operator_full_merge_fn (
 
     if (roaring_bitmap_is_empty(bmm)) {
         roaring_bitmap_free(bmm);
-        new_value_length = 0;
-        *success = 0;
+        *success = 1;
+        // new_value_length = &existing_value_length;
+        *new_value_length = 0;
         return NULL;
     }
 
+    roaring_bitmap_run_optimize(bmm);
+    roaring_bitmap_shrink_to_fit(bmm);
     size_t len = roaring_bitmap_portable_size_in_bytes(bmm);
-    char *result = (char *)calloc(len, sizeof(char));
+    char *result = malloc(len);
     if (!result) {
         roaring_bitmap_free(bmm);
-        new_value_length = 0;
-        *success = 0;
-        return NULL; 
+        // *success = 0;
+        new_value_length = &existing_value_length;
+        return (char*)existing_value;
     }
     roaring_bitmap_portable_serialize(bmm, result);
     roaring_bitmap_free(bmm);
@@ -245,7 +249,9 @@ const char* merge_operator_name_fn(void *state)
 
 void merge_operator_destructor_fn(void *state)
 {
-    free(state);
+    if (state) {
+        free(state);
+    }
 }
 
 rocksdb_mergeoperator_t* nflx_bitmap_merger(char *name) {
